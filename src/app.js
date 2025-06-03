ab752b3eecd88b32b187f89fb17c3ac95db1edf7
Sahal-pmk <Sahal.P.M.K@gds.ey.com>
2025-06-03 23:03:45 +0530

    created a express server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
-console.log("Hello World!");
+const express = require("express");
+const app = express();
+app.use("/sahal2",(req, res) =>{
+    console.log("sahal 3");
+    res.send("Hi sahal")
+    //next();
+})
+app.use("/",(req, res) =>{
+    console.log("sahal 4");
+    res.send("Hi sahal 2")
+})
+app.listen(7777, ()=>{
+    console.log("Listening on port 7777")
+})
+app.use("/", (req, res)=>{
+    res.send("Hi World 3")
+})
